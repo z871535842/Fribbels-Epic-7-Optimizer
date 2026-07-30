bbcce3628fa59e3693c237b625502c2fc34c724d
z871535842 <zhou6393@gmail.com>
2026-07-30 20:45:58 +0800

    更换jar包
    更换jar包

--- configs/webpack.config.base.js
+++ configs/webpack.config.base.js
@@ -5,34 +5,34 @@ const pkg = require('../app/package.json');
 const externals = pkg.dependencies || {};
 
 module.exports = {
-  externals: Object.keys(externals),
+    externals: Object.keys(externals),
 
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: { cacheDirectory: true },
-        },
-      },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: { cacheDirectory: true },
+                },
+            },
+        ],
+    },
+
+    output: {
+        path: path.join(__dirname, '..', 'app'),
+        libraryTarget: 'commonjs2',
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+        modules: [path.join(__dirname, '..', 'app'), 'node_modules'],
+    },
+
+    plugins: [
+        new webpack.EnvironmentPlugin({
+            NODE_ENV: 'production',
+        }),
     ],
-  },
-
-  output: {
-    path: path.join(__dirname, '..', 'app'),
-    libraryTarget: 'commonjs2',
-  },
-
-  resolve: {
-    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
-    modules: [path.join(__dirname, '..', 'app'), 'node_modules'],
-  },
-
-  plugins: [
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: 'production',
-    }),
-  ],
 };
